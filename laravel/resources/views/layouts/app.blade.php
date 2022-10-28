5ca30defb2ce8fb15a836156d6e1c33a995d3f0c
Johnnyy91 <claudiosardilli@gmail.com>
2022-10-28 17:24:38 +0200

    creation component

---
+++ laravel/resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.2/css/bootstrap.min.css" integrity="sha512-CpIKUSyh9QX2+zSdfGP+eWLx23C8Dj9/XmHjZY2uDtfkdLGo0uY12jgcnkX9vXOgYajEKb/jiw67EYm+kBf+6g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>LARAVEL-DC-COMICS</title>
+</head>
+<body>
+    @include('./partials.header')
+
+    @yield('content')
+
+    @include('./partials.footer')
+</body>
+</html>
---
+++ laravel/resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+ <section class="initial">
+        <div class="container d-flex justify-content-end align-items-center">
+            <p class="px-3">DC POWER VISA</p>
+            <p class="px-3">DC POWER VISA</p>
+        </div>
+    </section>
+
+<div class="container">
+    <section></section>
+    <section>
+        <img src="{{asset('images/jumbotron.jpg')}}" alt="jumbotron">
+    </section>
+</div>
