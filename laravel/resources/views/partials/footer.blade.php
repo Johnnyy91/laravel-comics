8dd5c8f359e1eef20fbaaff84825255fdd86bdb5
Johnnyy91 <claudiosardilli@gmail.com>
2022-10-31 18:11:28 +0100

    elaboration footer

---
+++ laravel/resources/views/partials/footer.blade.php
@@ -0,0 +1,66 @@
+<div class="footer">
+     <div class="link-list">
+        <div class="content-list">
+            <h3>DC COMICS</h3>
+            <ul>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+            </ul>
+            <h3>SHOP</h3>
+            <ul>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+            </ul>
+        </div>
+        <div class="content-list">
+            <h3>DC</h3>
+            <ul>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+            </ul>
+        </div>
+        <div class="content-list">
+            <h3>SITES</h3>
+            <ul>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Games</a></li>
+            </ul>
+        </div>
+     </div>
+     <div class="logo-footer">
+        <img src="{{asset('images/dc-logo.png')}}" alt="logo">
+     </div>
+  </div>
+
+  <div class="social-footer">
+    <div class="but-foot">
+        <button>SIGN-UP NOW!</button>
+    </div>
+    <div class="icon-social">
+        <h3>FOLLOW US</h3>
+        <img src="{{assets('img/footer-facebook.png')}}" alt="">
+        <img src="{{assets('img/footer-pinterest.png')}}" alt="">
+        <img src="{{assets('img/footer-twitter.png')}}" alt="">
+        <img src="{{assets('img/footer-youtube.png')}}" alt="">
+        <img src="{{assets('img/footer-periscope.png')}}" alt="">
+    </div>
+  </div>
